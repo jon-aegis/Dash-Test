f66032763680f4b37badaad869b82e35e480ada5
Michael Desgrosseilliers <michael.desgrosseilliers@maine.edu>
2019-11-10 22:11:34 -0500

    change line color

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
-from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -29,7 +28,7 @@ app.layout = html.Div([
                     opacity=0.7,
                     marker={
                         'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
+                        'line': {'width': 0.5, 'color': 'blue'}
                     },
                     name=i
                 ) for i in df.location.unique()
@@ -44,24 +43,6 @@ app.layout = html.Div([
         }
     )
 ])
-    # dcc.Graph(
-    #     id='example-graph',
-    #     figure={
-    #         'data': [
-    #             {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-    #             {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-    #         ],
-    #         'layout': {
-    #             'title': 'Dash Data Visualization'
-#             }
-#         }
-#     )
-# ])
-
-# @app.callback(dash.dependencies.Output('display-value', 'children'),
-#               [dash.dependencies.Input('dropdown', 'value')])
-# def display_value(value):
-#     return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
