bc94a4a84d2565491eb768b25e6262c8c359afc6
Michael Desgrosseilliers <michael.desgrosseilliers@maine.edu>
2019-11-10 20:36:23 -0500

    fixed dependencies

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
-
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
