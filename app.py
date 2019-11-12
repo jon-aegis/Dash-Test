dda3a0fe1447185b78de803c535a9874c6cf028b
Michael Desgrosseilliers <michael.desgrosseilliers@maine.edu>
2019-11-12 17:23:22 -0500

    changed font color of title

--- app.py
+++ app.py
@@ -18,12 +18,20 @@ df['DT'] = pd.to_datetime(df.DT, infer_datetime_format=True)
 df = df.replace(0, np.nan)
 df = df.dropna(how='all', axis=0)
 
+colors = {
+    'background': '#000000',
+    'text': '#0CDDD7'
+}
+
 app.layout = html.Div(children=[
-    html.H1(children='Sample of Our Data'),
+    html.H1(children='Sample of Our Data', style={
+            'textAlign': 'center',
+            'color': colors['text']}),
 
     html.Div(children='''
         Data: Real Power, Gas Use, and BTU Output
-    '''),
+    ''', style={'textAlign': 'center',
+                'color': colors['text']}),
     dcc.Graph(
         id='Real Power',
         figure={
@@ -38,6 +46,7 @@ app.layout = html.Div(children=[
                 ) for i in df.location.unique()
             ],
             'layout': go.Layout(
+                title= '',
                 xaxis={'title': 'Date'},
                 yaxis={'title': 'Real Power kW'},
                 margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
